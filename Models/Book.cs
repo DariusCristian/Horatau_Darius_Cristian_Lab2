ac9d6f66d1e0720930c038d0b4fe83751bbc9121
Admin <Admin@DARIUS>
2025-11-05 17:48:10 +0200

    Integrate Identity and Add Member/Borrowing Models
    Added DbSet properties for Member and Borrowing models in Horatau_Darius_Cristian_Lab2Context.cs. Updated Horatau_Darius_Cristian_Lab2.csproj to include ASP.NET Core Identity packages. Modified Horatau_Darius_Cristian_Lab2ContextModelSnapshot.cs to reflect new entities. Added navigation properties in Book model for Borrowing relationships. Updated _Layout.cshtml with navigation links for Members and Borrowings. Integrated ASP.NET Core Identity in Program.cs and configured LibraryIdentityContext. Updated appsettings.json with connection string for identity context. Created LibraryIdentityContext.cs for identity operations. Added identity management pages and error handling views. Implemented Razor Pages for CRUD operations on Member and Borrowing entities. Added migration files for database schema updates. Created Borrowing.cs and Member.cs models. Enhanced UI with partial views and validation scripts.

--- Models/Book.cs
+++ Models/Book.cs
@@ -19,7 +19,7 @@ namespace Horatau_Darius_Cristian_Lab2.Models
 
         public int? AuthorID { get; set; }
         public Author? Author { get; set; }
-
+        public ICollection<Borrowing>? Borrowings { get; set; }
         public ICollection<BookCategory>? BookCategories { get; set; }
     }
 }
---
+++ Pages/Borrowings/Details.cshtml.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using Horatau_Darius_Cristian_Lab2.Data;
+using Horatau_Darius_Cristian_Lab2.Models;
+
+namespace Horatau_Darius_Cristian_Lab2.Pages.Borrowings
+{
+    public class DetailsModel : PageModel
+    {
+        private readonly Horatau_Darius_Cristian_Lab2.Data.Horatau_Darius_Cristian_Lab2Context _context;
+
+        public DetailsModel(Horatau_Darius_Cristian_Lab2.Data.Horatau_Darius_Cristian_Lab2Context context)
+        {
+            _context = context;
+        }
+
+        public Borrowing Borrowing { get; set; } = default!;
+
+        public async Task<IActionResult> OnGetAsync(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var borrowing = await _context.Borrowing
+                .Include(b => b.Book)
+                 .ThenInclude(b => b.Author)
+                  .Include(b => b.Member)
+              .FirstOrDefaultAsync(m => m.ID == id);
+
+            if (borrowing == null)
+            {
+                return NotFound();
+            }
+            else
+            {
+                Borrowing = borrowing;
+            }
+            return Page();
+        }
+    }
+}
