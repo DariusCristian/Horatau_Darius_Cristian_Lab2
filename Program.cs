8c19fb4dc759a690eb36a14e963808405cbe178a
Admin <Admin@DARIUS>
2025-11-12 13:14:24 +0200

    Adaugat ca doar adminul sa poata accesta members

--- Program.cs
+++ Program.cs
@@ -3,7 +3,14 @@ using Microsoft.Extensions.DependencyInjection;
 using Horatau_Darius_Cristian_Lab2.Data;
 using Microsoft.AspNetCore.Identity;
 using Horatau_Darius_Cristian_Lab2.Pages.Books;
+
+
 var builder = WebApplication.CreateBuilder(args);
+builder.Services.AddAuthorization(options =>
+{
+    options.AddPolicy("AdminPolicy", policy =>
+   policy.RequireRole("Admin"));
+});
 
 // Add services to the container.
 builder.Services.AddRazorPages(options => {
@@ -13,6 +20,8 @@ builder.Services.AddRazorPages(options => {
     options.Conventions.AllowAnonymousToPage("/Books/Details");
     options.Conventions.AddPageRoute("/Books/Index", "/");
     options.Conventions.AddPageRoute("/Details", "/Books/Details/{id:int}");
+    options.Conventions.AuthorizeFolder("/Members", "AdminPolicy");
+
 });
 builder.Services.AddDbContext<Horatau_Darius_Cristian_Lab2Context>(options =>
     options.UseSqlServer(builder.Configuration.GetConnectionString("Horatau_Darius_Cristian_Lab2Context") ?? throw new InvalidOperationException("Connection string 'Horatau_Darius_Cristian_Lab2Context' not found.")));
